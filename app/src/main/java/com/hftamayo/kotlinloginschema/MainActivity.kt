6ed61721467e1a446f20ea88aa76830cdfff891e
hftamayo <hftamayo@gmail.com>
2023-09-28 11:55:04 -0600

    including an initial business logic

--- app/src/main/java/com/hftamayo/kotlinloginschema/MainActivity.kt
+++ app/src/main/java/com/hftamayo/kotlinloginschema/MainActivity.kt
@@ -2,10 +2,30 @@ package com.hftamayo.kotlinloginschema
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.hftamayo.kotlinloginschema.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var username : EditText
+    lateinit var password : EditText
+    lateinit var btnLogin : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener(View.OnClickListener {
+            if(binding.username.text.toString() == "user" && binding.password.text.toString() == "password"){
+                Toast.makeText(this, "Login successfully", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
     }
 }
